52622b600c37ab52430baae3457025e1ec293dec
Kai <Kai>
2015-05-09 20:23:39 +0800

    post crud done!

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,6 +15,27 @@ class PostsController < ApplicationController
     redirect_to :action => :index
   end
   
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+
+    redirect_to :action => :show, :id => @post
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to :action => :index
+  end
   ####
   private
 
